cb4389c8b5305ba5af64119e01b69475d75eab41
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-30 16:56:35 -0300

    Usuário de banco de dados e acesso à rede
    npm install mongodb
    MongoDB Atlas

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 //App Config
 const app = express();
 const port = process.env.PORT || 9000;
+const connection_url =
+  'mongodb+srv://admin:91944678Hugo@cluster0.048pwdo.mongodb.net/?retryWrites=true&w=majority';
 //Middleware
 //DB Config
+mongoose.connect(connection_url, {});
 //API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello TheWebDev'));
 //Listener
